3db7a4fbc7283a89b1e76300900e720e93eb6283
Monakiel <35536198+Monakiel@users.noreply.github.com>
2019-01-19 11:31:20 +0100

    update

--- game.js
+++ game.js
@@ -1,71 +1,95 @@
-let userClickedPattern = [];
-let gamePattern = [];
-let buttonColours = ["red", "blue", "green", "yellow"];
-let level = 0;
-let started = false;
+const buttonColours = ["red", "blue", "green", "yellow"];
 
-function nextSequence() {
-  let randomNumber = Math.floor((Math.random() * 4));
-  let randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
-  level++;
-  $("h1").text("Level " + level);
+const gamePattern = [];
+const userClickedPattern = [];
+
+function nextSequence()
+{
+	const randomNumber = Math.floor((Math.random() * buttonColours.length));
+	const randomChosenColour = buttonColours[randomNumber];
+
+	gamePattern.push(randomChosenColour);
+	$('li[class=' + randomChosenColour + ']').fadeOut(100).fadeIn(100);
+	playSound(randomChosenColour);
+	$("h1").text("Level " + gamePattern.length);
 }
 
-$(".btn").click(function() {
-  let userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-  checkAnswer(userClickedPattern.length - 1);
+function playSound(name)
+{
+	const audio = new Audio("sounds/" + name + '.mp3');
+	audio.play();
+}
+
+function animatePress(currentColour)
+{
+	const color = $('li[class=' + currentColour + ']');
+
+	$('li[class=' + currentColour + ']')
+		.addClass("pressed")
+		.delay(100)
+		.queue(function(go) {
+			$(this).removeClass("pressed");
+			go();
+		})
+	;
+}
+
+function checkAnswer()
+{
+	const idx = userClickedPattern.length - 1;
+
+	if (gamePattern[idx] === userClickedPattern[idx])
+	{
+		if (gamePattern.length === userClickedPattern.length)
+		{
+			// On ajoute donc une nouvelle couleur et on reset les couleurs cliquées;
+			// Comme gamePattern et userClickedPattern sont des "const",
+			// on ne peut pas faire "gamePattern = []".
+			// Du coup on utilise une feinte pour vider le tableau sans le "réassigner".
+			userClickedPattern.length = 0;
+			setTimeout(nextSequence, 1000);
+		}
+	}
+	else
+	{
+		playSound('wrong');
+		$("body")
+			.addClass("game-over")
+			.delay(200)
+			.queue(function(go) {
+				$(this).removeClass("game-over");
+				go();
+			})
+		;
+		startOver();
+	}
+}
+
+function startOver()
+{
+	$("h1").text("Game Over, Press Any Key to Restart");
+	gamePattern.length = 0;
+	userClickedPattern.length = 0;
+}
+
+$(document).keypress(() => {
+	if (!gamePattern.length)
+	{
+		nextSequence();
+	}
 });
 
-function playSound(name) {
-  let audio = new Audio("sounds/" + name + '.mp3');
-  audio.play();
-}
+$(document).ready(() => {
 
-function animatePress(currentColour) {
-  $("#" + currentColour).addClass("pressed").delay(100).queue(function(go) {
-    $(this).removeClass("pressed");
-    go();
-  });
-}
+	$('li').click(function() {
+		if (!gamePattern.length)
+			return;
 
-$(document).keypress(function() {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
+		const userChosenColour = $(this).attr('class');
+
+		userClickedPattern.push(userChosenColour);
+		playSound(userChosenColour);
+		animatePress(userChosenColour);
+		checkAnswer();
+	});
 });
-
-function checkAnswer(currentLevel) {
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    if (gamePattern.length === userClickedPattern.length) {
-      setTimeout(function() {
-        nextSequence();
-      }, 1000);
-      userClickedPattern = [];
-    } else {
-
-    }
-  } else {
-    let audio = new Audio("sounds/wrong.mp3");
-    audio.play();
-    $("body").addClass("game-over").delay(200).queue(function(go) {
-      $(this).removeClass("game-over");
-      go();
-    });
-    $("h1").text("Game Over, Press Any Key to Restart");
-    startOver();
-  }
-}
-
-function startOver() {
-  level = 0;
-  gamePattern = [];
-  started = false;
-}
